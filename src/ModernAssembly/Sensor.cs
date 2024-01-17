347a296727f3024957ef37539a2b067ebf916dc0
chenyulin <chenyulin@sjtu.edu.cn>
2024-01-17 12:13:17 +0800

    feat. add sensor module

---
+++ src/ModernAssembly/Sensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modern
+{
+    class Sensor : Unit
+    {
+        public MToggle OnBoard;
+
+        public override void SafeAwake()
+        {
+            OnBoard = AddToggle("On Board", "OnBoard", "On Board", false);
+            SensorSafeAwake();
+        }
+
+        public virtual void SensorSafeAwake()
+        {
+
+        }
+    }
+}
