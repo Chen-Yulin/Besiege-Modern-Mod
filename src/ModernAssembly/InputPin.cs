b2d5f3aec25ea92d3853fa98286363a325d945e3
chenyulin <chenyulin@sjtu.edu.cn>
2024-01-22 12:28:07 +0800

    feat. add basic input output pin

---
+++ src/ModernAssembly/InputPin.cs
@@ -0,0 +1,29 @@
+﻿using Modding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Modern
+{
+    public class InputPin : Unit
+    {
+        public Transform Vis;
+
+        public override void OnSimulateStart()
+        {
+            name = "Input Pin";
+            InputNum = 0;
+            ControlNum = 0;
+            OutputNum = 1;
+            Vis = transform.Find("Vis");
+            Outputs.Add(Vis.gameObject.AddComponent<Port>());
+            Outputs[0].InitPort(this, true, Data.DataType.Any, 0, 1, false, false);
+        }
+        public override void OnUnitSimulateStart()
+        {
+            name = "Input Pin";
+        }
+    }
+}
---
+++ src/ModernAssembly/OutputPin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Modern
+{
+    internal class OutputPin : Unit
+    {
+        public Transform Vis;
+
+        public override void OnSimulateStart()
+        {
+            name = "Output Pin";
+            InputNum = 1;
+            ControlNum = 0;
+            OutputNum = 0;
+            Vis = transform.Find("Vis");
+            Inputs.Add(Vis.gameObject.AddComponent<Port>());
+            Inputs[0].InitPort(this, false, Data.DataType.Any, 0, 1, false, false);
+        }
+        public override void OnUnitSimulateStart()
+        {
+            name = "Output Pin";
+        }
+        public override void UpdateUnit()
+        {
+            base.UpdateUnit();
+        }
+    }
+}
