fcef7980535e488683a8485958b695bf36850bd3
chenyulin <chenyulin@sjtu.edu.cn>
2024-03-13 16:51:58 +0800

    feat. enable sensor to receive parameter

--- src/ModernAssembly/RadarSensor.cs
+++ src/ModernAssembly/RadarSensor.cs
@@ -137,9 +137,20 @@ namespace Modern
     public class RadarSensor : Sensor
     {
         public Beam beam;
+
+        public float power = 1;
+
+        public void AdjustBeam(float dist)
+        {
+            beam.transform.localScale = new Vector3(1 / transform.lossyScale.x / Mathf.Sqrt(dist), 1 / transform.lossyScale.y / Mathf.Sqrt(dist), dist / transform.lossyScale.z) * power;
+        }
         public override string GetName()
         {
             return "Radar Sensor";
+        }
+        public override bool needPara()
+        {
+            return true;
         }
         public override void SensorSimulateStart()
         {
@@ -147,9 +158,15 @@ namespace Modern
             BeamObject.transform.parent = transform;
             BeamObject.transform.localPosition = Vector3.zero;
             BeamObject.transform.localRotation = Quaternion.identity;
-            BeamObject.transform.localScale = new Vector3(1 / transform.lossyScale.x, 1 / transform.lossyScale.y, 1 / transform.lossyScale.z);
+            BeamObject.transform.localScale = new Vector3(power / transform.lossyScale.x, power / transform.lossyScale.y, power / transform.lossyScale.z);
             beam = BeamObject.AddComponent<Beam>();
             beam.InitBeam(this);
+
+
+            if (onboard)
+            {
+                Inputs[0].Type = Data.DataType.Float;
+            }
         }
         public override Data SensorGenerate()
         {
@@ -175,6 +192,27 @@ namespace Modern
                 return new Data(pkg);
             }
             
+        }
+
+        public override void SensorUpdatePara()
+        {
+            float dist = power;
+            if (Inputs[0].MyData.Type != Data.DataType.Null)
+            {
+                dist = Inputs[0].MyData.Flt;
+            }
+            
+            AdjustBeam(dist);
+        }
+        public override void WirelessSensorUpdatePara(Data data)
+        {
+            float dist = power;
+            if (data.Type != Data.DataType.Null)
+            {
+                dist = data.Flt;
+            }
+
+            AdjustBeam(dist);
         }
     }
 }
--- src/ModernAssembly/WirelessManager.cs
+++ src/ModernAssembly/WirelessManager.cs
@@ -9,30 +9,52 @@ namespace Modern
     {
         public override string Name { get; } = "Wireless Manager";
 
-        public Dictionary<string, List<Port>> WirelessMapper = new Dictionary<string, List<Port>>();
+        public Dictionary<string, List<Port>> WirelessMapperPort = new Dictionary<string, List<Port>>();
+        public Dictionary<string, List<Unit>> WirelessMapperUnit = new Dictionary<string, List<Unit>>();
 
         public void RegisterPort(string channel, Port port)
         {
-            if (!WirelessMapper.ContainsKey(channel))
+            if (!WirelessMapperPort.ContainsKey(channel))
             {
-                WirelessMapper.Add(channel, new List<Port>());
+                WirelessMapperPort.Add(channel, new List<Port>());
             }
-            WirelessMapper[channel].Add(port);
+            WirelessMapperPort[channel].Add(port);
+        }
+        public void RegisterUnit(string channel, Unit unit)
+        {
+            if (!WirelessMapperUnit.ContainsKey(channel))
+            {
+                WirelessMapperUnit.Add(channel, new List<Unit>());
+            }
+            WirelessMapperUnit[channel].Add(unit);
         }
         public void UnregisterChannel(string channel)
         {
-            if (WirelessMapper.ContainsKey(channel))
+            if (WirelessMapperPort.ContainsKey(channel))
             {
-                WirelessMapper.Remove(channel);
+                WirelessMapperPort.Remove(channel);
+                return;
+            }
+            if (WirelessMapperUnit.ContainsKey(channel))
+            {
+                WirelessMapperUnit.Remove(channel);
+                return;
             }
         }
         public void PassData(string key, Data data)
         {
-            if (WirelessMapper.ContainsKey(key))
+            if (WirelessMapperPort.ContainsKey(key))
             {
-                foreach (var port in WirelessMapper[key])
+                foreach (var port in WirelessMapperPort[key])
                 {
                     port.MyData = data;
+                }
+            }
+            if (WirelessMapperUnit.ContainsKey(key))
+            {
+                foreach (var unit in WirelessMapperUnit[key])
+                {
+                    unit.WirelessReceiveData(data);
                 }
             }
         }
