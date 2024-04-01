d77ed096e4c626b7782561bf7bd5867c38db8b5e
chenyulin <chenyulin@sjtu.edu.cn>
2024-04-01 15:59:59 +0800

    feat. add auto reset to toggle

--- src/ModernAssembly/Switch.cs
+++ src/ModernAssembly/Switch.cs
@@ -10,6 +10,9 @@ namespace Modern
     {
         public MKey SwitchKey;
         public MToggle DefaultOn;
+        public MToggle AutoReturn;
+
+        public bool useEmulate = false;
 
         public Transform Vis;
         private bool on = false;
@@ -47,6 +50,7 @@ namespace Modern
         {
             SwitchKey = AddKey("Switch Key", "Switch Key", KeyCode.T);
             DefaultOn = AddToggle("Default On", "Default On", false);
+            AutoReturn = AddToggle("Auto Reset", "Auto Reset", false);
         }
 
         public override void SensorSimulateStart()
@@ -61,17 +65,48 @@ namespace Modern
 
         public override void SensorSimulateFixedUpdate()
         {
-            if (SwitchKey.EmulationPressed())
+            if (AutoReturn.isDefaultValue)
             {
-                On = !On;
+                if (SwitchKey.EmulationPressed())
+                {
+                    On = !On;
+                }
             }
+            else
+            {
+                if (SwitchKey.EmulationHeld())
+                {
+                    On = true;
+                    useEmulate = true;
+                }
+                else if (useEmulate)
+                {
+                    On = false;
+                }
+            }
+            
 
         }
         public override void SensorSimulateUpdate()
         {
-            if (SwitchKey.IsPressed)
+            if (AutoReturn.isDefaultValue)
             {
-                On = !On;
+                if (SwitchKey.IsPressed)
+                {
+                    On = !On;
+                }
+            }
+            else
+            {
+                if (SwitchKey.IsHeld)
+                {
+                    On = true;
+                    useEmulate = false;
+                }
+                else if (!useEmulate)
+                {
+                    On = false;
+                }
             }
         }
 
